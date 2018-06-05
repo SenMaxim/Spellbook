b4f787d48b583adc8a7761901f3c5b319a969a78
Maxim Sen <maxsen06@gmail.com>
2018-06-05 10:32:34 -0400

    hw1

--- index.js
+++ index.js
@@ -1,1 +1,13 @@
-console.log('Testing');
+const header1 = document.querySelector('h1')
+const header2 = document.getElementById('book2')
+const header3 = document.getElementById('book3')
+const button = document.querySelector('button')
+
+button.addEventListener('click', function() {
+    console.log('event handled')
+  header2.textContent = 'Spellbook2 text changed!'
+})
+
+var updateSpellBook3 = function(form) {
+  header3.textContent = form.input.value;
+}
